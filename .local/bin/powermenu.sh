ec561bfd9ff9b407ad964fd47b16215a2dda1cf7
Kirill <kitrofimov39@gmail.com>
2024-09-29 19:30:06 +0500

    Fix reboot in powermenu

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -4,7 +4,7 @@ CHOSEN=$(printf "Suspend\nReboot\nShutdown\nLog Out" | rofi -dmenu)
 
 case "$CHOSEN" in
 	"Suspend") systemctl suspend ;;
-	"Reboot") reboot ;;
+	"Reboot") systemctl reboot ;;
 	"Shutdown") systemctl poweroff ;;
 	"Log Out") i3-msg exit ;;
 	*) exit 1 ;;
